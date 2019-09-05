425aa2e6fe172095242a850660f60069cc70cc09
Seamus McKinsey <seamus.mckinsey@gmail.com>
2019-09-04 21:37:29 -0700

    expand QWI metrics to get industry breakdown

--- R/initial_exploration.R
+++ R/initial_exploration.R
@@ -1,7 +1,7 @@
 library(tidyverse)
 library(censusapi)
 library(skimr)
-
+library(janitor)
 
 # functions ---------------------------------------------------------------
 
@@ -19,6 +19,25 @@ as_date_year_qtr <- function(df, variable, format = "%Y-Q%q", ...) {
       date      = zoo::as.Date(year_qtr) 
     )
 }
+
+
+get_qwi_metrics <- function(name = "timeseries/qwi/se", 
+                            vars = c(qwi_numeric_variables, "education"), 
+                            region = "county:*", 
+                            regionin = "state:06",
+                            time = "from 2000 to 2018", ...) {
+  tidy_get_census(
+    name = name,
+    vars = vars,
+    region = region,
+    regionin = regionin,
+    time = time,
+    ...
+  )
+}
+
+safely_get_qwi_metrics <- safely(get_qwi_metrics)
+
 
 
 # apis --------------------------------------------------------------------
@@ -48,7 +67,7 @@ raw_qwi_se <- tidy_get_census(
   time = "from 2000 to 2018", # get data from 2000 to 2018
   sex = "1", # get both male and female
   sex = "2" # get both male and female
-  ) 
+) 
 
 #clean raw QWI SE data
 clean_qwi_se <- raw_qwi_se %>% 
@@ -56,7 +75,72 @@ clean_qwi_se <- raw_qwi_se %>%
   as_date_year_qtr(time) %>% 
   # convert numeric variables from characters
   mutate_at(vars(qwi_numeric_variables), as.numeric) 
- 
+
 # quick exploration of QWI SE dataset
 glimpse(clean_qwi_se)
 skim(clean_qwi_se)
+
+# get industry breakdown --------------------------------------------------
+
+# get all variables by sex and education from census API
+raw_qwi_se_industry_51 <- tidy_get_census(
+  name = "timeseries/qwi/se", 
+  vars = c(qwi_numeric_variables, "education"), 
+  region = "county:*", # get all counties
+  regionin = "state:06", # CA
+  time = "from 2000 to 2018", # get data from 2000 to 2018
+  sex = "1", # get both male and female
+  sex = "2", # get both male and female
+  industry = 51
+) 
+
+raw_qwi_se_industry_51 <- get_qwi_metrics(
+  sex = "1", sex = "2",
+  industry = 51
+)
+
+# https://www.census.gov/eos/www/naics/2017NAICS/2017_NAICS_Structure_Summary_Table.xlsx
+industries <- tibble::tribble(
+  ~Sector,                                                                      ~Name,
+  "11",                               "Agriculture, Forestry, Fishing and Hunting",
+  "21",                            "Mining, Quarrying, and Oil and Gas Extraction",
+  "22",                                                                "Utilities",
+  "23",                                                             "Construction",
+  "31-33",                                                            "Manufacturing",
+  "42",                                                          "Wholesale Trade",
+  "44-45",                                                             "Retail Trade",
+  "48-49",                                           "Transportation and Warehousing",
+  "51",                                                              "Information",
+  "52",                                                    "Finance and Insurance",
+  "53",                                       "Real Estate and Rental and Leasing",
+  "54",                         "Professional, Scientific, and Technical Services",
+  "55",                                  "Management of Companies and Enterprises",
+  "56", "Administrative and Support and Waste Management and Remediation Services",
+  "61",                                                     "Educational Services",
+  "62",                                        "Health Care and Social Assistance",
+  "71",                                      "Arts, Entertainment, and Recreation",
+  "72",                                          "Accommodation and Food Services",
+  "81",                            "Other Services (except Public Administration)",
+  "92",                                                    "Public Administration"
+) %>% 
+  clean_names() %>% 
+  mutate(sector = parse_number(sector))
+
+qwi_by_industry <- industries %>% 
+  mutate(
+    df = map(sector, ~ {
+      Sys.sleep(10)
+      safely_get_qwi_metrics(industry = .x, sex = "1", sex = "2")
+    }
+    )
+  ) %>% 
+  compact()
+
+valid_qwi_by_industry <- qwi_by_industry %>% 
+  # unnesting list column
+  unnest(df) %>% 
+  # filtering on actual returned tibbles (not errors or null values)
+  filter(df %>% map(is_tibble) %>% map_lgl(any)) %>% 
+  unnest(df)
+
+write_csv(valid_qwi_by_industry, "~/Dropbox/EOQ/Data/QWI/qwi_se_by_industry.csv.gz")
