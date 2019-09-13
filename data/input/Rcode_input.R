fe713ca4dd89abd067a0506f1cd4fec51752ab2e
Matthew Staiger <matthewstaiger@gmail.com>
2019-09-12 22:58:21 -0400

    update

--- data/input/Rcode_input.R
+++ data/input/Rcode_input.R
@@ -17,17 +17,22 @@ install.packages("tidyverse")
 
 
 
+
 #---------------------------------------------------------------------------------------------------------#
 #CPS: birng in and clean data from CPS
 #---------------------------------------------------------------------------------------------------------#
+#bring in data
 setwd(file.path(cps_dir))
-cps<-read.csv(file="cps_00018.csv",colClasses=c(COUNTY="character"),header=TRUE, sep=",")
+cps<-read.csv(file="cps_00018.csv",colClasses=c(COUNTY="character",STATEFIP="character"),header=TRUE, sep=",")
 setwd(file.path(code_dir))
 attach(cps)
-cps<-cps[ which(STATEFIP=="6" & nchar(COUNTY)==4) , ]
+#clean data
+cps<-cps[ which(STATEFIP=="6" & nchar(COUNTY)>=4) , ]
+cps$STATEFIP <- sub("^", "0", cps$STATEFIP)
 cps$COUNTY <- sub("^", "0", cps$COUNTY)
+#rename variables
+colnames(cps)[colnames(cps)=="STATEFIP"] <- "STATE"
+#save final data
+finalvars<-c("STATE","COUNTY")
+cps_final<-cps[finalvars]
 detach(cps)
-
-
-
-
