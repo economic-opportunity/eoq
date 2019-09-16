84dca8ca795fa4f69e8a07ae88585891eda36d4f
Matthew Staiger <matthewstaiger@gmail.com>
2019-09-16 11:08:23 -0400

    temp save

--- data/input/Rcode_input.R
+++ data/input/Rcode_input.R
@@ -1,41 +1,45 @@
-#THIS CODE INPUTS THE DATA
+#---------------------------------------------------------------------------------------------------------#
+#THIS SCRIPT INPUTS INDIVIDUAL LEVEL DATA FROM THE CPS
+#---------------------------------------------------------------------------------------------------------#
+#clear all
 rm(list = ls())
 
 #define working directory (personalize)
-code_dir<-"/Users/matthewstaiger/Google Drive EOQ/EOQ/Code/MS/eoq/data/input"
+personal_dir<-"/Users/matthewstaiger/Google Drive EOQ"
 
-#defin directories for code
-cps_dir<-"/Users/matthewstaiger/Google Drive EOQ/EOQ/Data/CPS"
-
+#define directory for input code
+code_dir<-"EOQ/Code/MS/eoq/data/input"
+#defin directories for data
+cps_dir<-"EOQ/Data/CPS"
+intermediate_dir<-"EOQ/Data/intermediate"
+#define directory for R packages
+packages_dir<-"EOQ/Code/MS/eoq/renv"
+  
 #set working directory
-setwd(file.path(code_dir))
+setwd(file.path(personal_dir,code_dir))
 getwd()
 
-#install packages
-install.packages('ipumsr')
-install.packages("tidyverse")
-
-
-
+#package managements
+renv::restore()
 
 #---------------------------------------------------------------------------------------------------------#
 #CPS: birng in and clean data from CPS
 #---------------------------------------------------------------------------------------------------------#
 #bring in data
-setwd(file.path(cps_dir))
-cps<-read.csv(file="cps_00018.csv",colClasses=c(COUNTY="character",STATEFIP="character"),header=TRUE, sep=",")
-setwd(file.path(code_dir))
+cps<-read.csv(file=paste(file.path(personal_dir,cps_dir),"/cps_00018.csv",sep=""),colClasses=c(COUNTY="character",STATEFIP="character"),header=TRUE, sep=",")
 attach(cps)
 #clean data
 cps<-cps[ which(STATEFIP=="6" & nchar(COUNTY)>=4) , ]
 cps$STATEFIP <- sub("^", "0", cps$STATEFIP)
 cps$COUNTY <- substr(cps$COUNTY,2,4)
 #rename variables
+cps<-dplyr::rename(cps,state=STATEFIP)
+
 colnames(cps)[colnames(cps)=="STATEFIP"] <- "state"
 colnames(cps)[colnames(cps)=="COUNTY"] <- "county"
 colnames(cps)[colnames(cps)=="YEAR"] <- "year"
 #save final data
 finalvars<-c("state","county","year")
 cps_final<-cps[finalvars]
+write.csv(cps_final, file = paste(file.path(personal_dir,intermediate_dir),"/cps.csv",sep=""))
 detach(cps)
-
--- data/transform/transformdata.R
+++ data/transform/transformdata.R
@@ -1,1 +1,33 @@
-#transform data
+#---------------------------------------------------------------------------------------------------------#
+#THIS SCRIPT PRODUCES THE ANLAYSIS DATASET
+#---------------------------------------------------------------------------------------------------------#
+#clear all#clear all
+rm(list = ls())
+
+#define working directory (personalize)
+personal_dir<-"/Users/matthewstaiger/Google Drive EOQ"
+
+#define directory for input code
+code_dir<-"EOQ/Code/MS/eoq/data/input"
+#defin directories for data
+cps_dir<-"EOQ/Data/CPS"
+bls_dir<-"EOQ/Data/BLS"
+qwi_dir<-"EOQ/Data/QWI"
+
+intermediate_dir<-"/EOQ/Data/intermediate"
+
+#set working directory
+setwd(file.path(personal_dir,code_dir))
+getwd()
+
+#install packages
+install.packages('ipumsr')
+install.packages("tidyverse")
+
+
+bls<-read.csv(file=paste(file.path(personal_dir,bls_dir),"/California_All_Counties_Unemployment_Rate_2010-2019.csv",sep=""),header=TRUE, sep=",")
+
+qwi<-read.csv(file=paste(file.path(personal_dir,qwi_dir),"/qwi_se_quarterly.csv",sep=""),header=TRUE, sep=",",colClasses=c(county="character",state="character"))
+
+
+
