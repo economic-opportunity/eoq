5f1a88fbc6ecaf069d50df62a9a14cd2c1e95036
Matthew Staiger <matthewstaiger@gmail.com>
2019-09-16 11:36:00 -0400

    temp save

--- data/input/Rcode_input.R
+++ data/input/Rcode_input.R
@@ -28,18 +28,25 @@ renv::restore()
 #bring in data
 cps<-read.csv(file=paste(file.path(personal_dir,cps_dir),"/cps_00018.csv",sep=""),colClasses=c(COUNTY="character",STATEFIP="character"),header=TRUE, sep=",")
 attach(cps)
-#clean data
+#subset data to population of interest
+cps<-dplyr::filter(cps,EMPSTAT==10)
+cps<-dplyr::filter(cps,INCWAGE!=9999999,INCWAGE!=9999998,INCWAGE!=0)
 cps<-cps[ which(STATEFIP=="6" & nchar(COUNTY)>=4) , ]
 cps$STATEFIP <- sub("^", "0", cps$STATEFIP)
 cps$COUNTY <- substr(cps$COUNTY,2,4)
 #rename variables
 cps<-dplyr::rename(cps,state=STATEFIP)
-
-colnames(cps)[colnames(cps)=="STATEFIP"] <- "state"
-colnames(cps)[colnames(cps)=="COUNTY"] <- "county"
-colnames(cps)[colnames(cps)=="YEAR"] <- "year"
+cps<-dplyr::rename(cps,county=COUNTY)
+cps<-dplyr::rename(cps,year=YEAR)
+cps<-dplyr::rename(cps,month=MONTH)
+cps<-dplyr::rename(cps,incwage=INCWAGE)
+cps<-dplyr::rename(cps,occ=OCC)
+cps<-dplyr::rename(cps,ind=IND)
+cps<-dplyr::rename(cps,age=AGE)
+cps<-dplyr::rename(cps,sex=SEX)
+cps<-dplyr::rename(cps,race=RACE)
 #save final data
-finalvars<-c("state","county","year")
+finalvars<-c("state","county","year","month","incwage","occ","ind","age","sex","race")
 cps_final<-cps[finalvars]
 write.csv(cps_final, file = paste(file.path(personal_dir,intermediate_dir),"/cps.csv",sep=""))
 detach(cps)
